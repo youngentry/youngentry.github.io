9f681f8a5453d3b44ba1f1c3e8eee0a1b07d4fc9
young <gentry_@naver.com>
2022-05-27 18:23:47 +0900

    Add files via upload

--- trn01/js/main.js
+++ trn01/js/main.js
@@ -1,8 +1,14 @@
 $(function () {
+  $(".main_slider").on("init reInit afterChange", function () {
+    var here = $(".slick-current");
+    here.addClass("on").siblings().removeClass("on");
+  });
+  
   $(".main_slider").slick({
     arrows: false,
     autoplay: true,
     pauseOnHover: false,
     pauseOnFocus: false,
+    fade: true,
   });
 });
